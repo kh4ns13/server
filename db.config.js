3a33a5c5710b0e42624212cdd177ca63a5c5eea7
kh4ns13 <142240931+kh4ns13@users.noreply.github.com>
2023-08-14 17:57:49 +0300

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-cjd3v5anip6c73adclkg-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "8fWDUNYGQJZjq8DK9G2WihyqbXGFerue",
+    DB: "kh4ns13",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
